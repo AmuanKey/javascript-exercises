a4fa0f26625a7044e5f94e45584910d8a1b69ed3
Amuan <amuanistaken@gmail.com>
2024-06-28 00:57:13 +0700

    Had to repeating a string with a function. Simple and fun

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
+const repeatString = function(word,num) {
+let newWord ="";
+
+
+    if(num < 0){
+        newWord += 'ERROR'; 
+    }else{
+        for (let i = 0; i < num; i++) {
+        newWord += word;
+
+    }
+    
+}
+
+return newWord;
+
 
 };
-
+console.log(repeatString("hey",-1));
 // Do not edit below this line
 module.exports = repeatString;
