00d6686d86d2694eca3721874ce1c6cb833ef65d
Amuan <amuanistaken@gmail.com>
2024-06-30 17:34:24 +0700

    technically this code solve the problem but when tested it didn't pass I don't know why

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
 
+const reverseString = function(word) {
+let reversearray = [];
+for (let i = word.length-1; i >= 0; i--) {
+    reversearray.push(word[i]);
+    }
+let reverseWord = reversearray.join("");
+console.log(reverseWord);
 };
 
+
+reverseString("123! abc! Hello, Odinite.");
 // Do not edit below this line
 module.exports = reverseString;
