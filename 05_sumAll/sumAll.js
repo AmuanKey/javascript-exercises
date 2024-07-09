30da96a3b5cb747b4ff9de0cbc50dfb093181212
Amuan <amuanistaken@gmail.com>
2024-07-09 22:41:20 +0700

    today I solve 05 and it is quite fun and cool

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let sum = 0;
+    let bigNum = 0;
+    let smallNum = 0;
+    if (a < 0 || b < 0 || typeof(a) != "number" || typeof(b) != "number"){
+        return "ERROR";
+    }
+    else{
+    if (a>b){
+         bigNum = a;
+         smallNum = b;
+    }else{
+bigNum = b;
+smallNum = a;
+    }
+for (let i = smallNum; i <= bigNum; i++) {
+    sum = sum + i;
+  
+    
+}}
+return sum;
 };
-
+console.log(sumAll("5",1));
 // Do not edit below this line
 module.exports = sumAll;
