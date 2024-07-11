c0ba13d9a26a7e5a47842f8ccee57f9f9aecfbc9
Amuan <amuanistaken@gmail.com>
2024-07-11 22:23:45 +0700

    this is my solution to 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,24 @@
-const leapYears = function() {
+const leapYears = function(y) {
+if (y % 4 == 0) {
+    if(y % 100 == 0){
+        if(y % 400 == 0){
+            return true;
+        }
+        else{
+            return false;
 
+        }
+    }
+    else{
+        return true;
+
+    }
+}
+
+else{
+    return false;
+}
 };
-
+console.log(leapYears(2012));
 // Do not edit below this line
 module.exports = leapYears;
