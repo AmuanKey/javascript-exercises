97f1ddcbf3afde21d03f016348ed1f917a26ba20
Amuan <amuanistaken@gmail.com>
2024-07-12 19:57:19 +0700

    This is 07 hopefully. I don't know what will be the next one be or will this be even work?

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let newTemp = (f-32)*(5/9);
+  let c = Math.round(newTemp*10)/10;
+  return c;
 };
 
-const convertToFahrenheit = function() {
+
+
+const convertToFahrenheit = function(c) {
+  let newTemp = (c*(9/5))+32;
+  let f = Math.round(newTemp*10)/10;
+  return f;
 };
+console.log(convertToCelsius(-100));
+//console.log(convertToFahrenheit(40));
 
 // Do not edit below this line
 module.exports = {
