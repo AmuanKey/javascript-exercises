735876f18e7c175c45e8dcb215642b3a1bd8aab4
Amuan <amuanistaken@gmail.com>
2024-07-12 22:15:49 +0700

    11 got it I don't think its right but it's ok for now

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,24 @@
-const getTheTitles = function() {
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
 
+
+const getTheTitles = function(n) {
+    let arr = [];
+for (let i = 0; i < n.length; i++) {
+    const element = n[i].title;
+    arr.push(element);
+    
+}
+return arr;
 };
-
+console.log(getTheTitles(books));
 // Do not edit below this line
 module.exports = getTheTitles;
