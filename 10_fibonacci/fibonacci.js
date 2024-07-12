988955a3c1c7a1477707a2bab3ea2b1ad8e4dfc7
Amuan <amuanistaken@gmail.com>
2024-07-12 22:00:19 +0700

    this is 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) {
+        return "OOPS";
+    }else {
 
+    
+ let n1=0; let n2= 1; let nextNum;
+ let arr = [];
+for (let i = 0; i <= n; i++) {
+    nextNum = n1 + n2;
+    arr.push(n1);
+    n1=n2;
+    n2=nextNum;
+
+}
+return arr[arr.length-1];
+    }
 };
-
+console.log(fibonacci(-1));
 // Do not edit below this line
 module.exports = fibonacci;
