9714a6a3ef041ce79e26750cc56776182ae5c914
Amuan <amuanistaken@gmail.com>
2024-07-12 21:35:16 +0700

    this is the solution to 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+let newStr=  str.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~" "]/g,'').toLowerCase();
+let reverseStr = newStr.split('').reverse().join('');
+
+if(reverseStr == newStr){
+    return true;
+}
+else{
+    return false;
+}
+
 
 };
-
+console.log(palindromes("??rac3e3car!!!"));
 // Do not edit below this line
 module.exports = palindromes;
